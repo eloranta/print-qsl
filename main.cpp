87c9e7c34bb7ce63344bc649f07d4d8165f81ca9
eloranta <hannu.eloranta@gmail.com>
2018-01-09 18:26:56 +0200

    simple printing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QPrinter>
+#include <QPrintDialog>
+#include <QPainter>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
 
-    return a.exec();
+    QPrinter printer;
+    QPrintDialog *dialog = new QPrintDialog(&printer);
+    dialog->setWindowTitle("Print Document");
+    if (dialog->exec() != QDialog::Accepted)
+            return -1;
+
+    QPainter painter;
+    painter.begin(&printer);
+    painter.drawRect(0, 0, 100, 100);   //drawText(100, 100, 500, 500, Qt::AlignLeft|Qt::AlignTop, "hello");
+
+
+    painter.end();
+
+    return app.exec();
 }
